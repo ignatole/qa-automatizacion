17b80bd9a3ab12a16e7128859431adc867de626a
Ignacio Toledo Roll <rolltoledoignacio@gmail.com>
2025-03-13 14:53:30 -0300

    Corrijo el POM y el nombre de un test

--- src/test/java/com/enviopack/tests/TableReaderTest.java
+++ src/test/java/com/enviopack/tests/TableReaderTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import org.testng.annotations.Test;
 
-public class TableReaderTests extends BaseTest {
+public class TableReaderTest extends BaseTest {
     @Test
     public void TestTableReaderHeaders() {
         loginAs("admin");
